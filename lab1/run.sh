8239f5f0d4922663f38d6230cc826cd9b3396c26
zandent <simon.zhao@mail.utoronto.ca>
2020-10-09 02:23:33 -0400

    add report.txt

--- lab1/run.sh
+++ lab1/run.sh
@@ -5,7 +5,3 @@ echo "running 32768 8192"
 out/main_v$1 32768 8192
 echo "running 30 8947850"
 out/main_v$1 30 8947850
-echo "running 1 333333333"
-out/main_v$1 1 333333333
-echo "running 3 111111111"
-out/main_v$1 3 111111111
