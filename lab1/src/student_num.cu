3e4b1a02c9ed09a3ec6fd084cd4a6cf44b670085
zandent <simon.zhao@mail.utoronto.ca>
2020-10-09 19:58:18 -0400

    finish v4

--- lab1/src/student_num.cu
+++ lab1/src/student_num.cu
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <cuda_runtime.h>
 #define NUM_STREAMS 8 
+//For time log by callback function
 double timeStampB;
 double timeStampC;
 double timeStampD;
@@ -11,6 +12,7 @@ double getTimeStamp() {
  gettimeofday( &tv, NULL ) ;
  return (double) tv.tv_usec/1000000 + tv.tv_sec ;
 }
+//The three callback functions are used to called when finishing memcpyasyc and kernal completion
 void myCallBackB(cudaStream_t stream,cudaError_t status, void*  userData ){
  timeStampB=getTimeStamp();
 }
@@ -20,6 +22,7 @@ void myCallBackC(cudaStream_t stream,cudaError_t status, void*  userData ){
 void myCallBackD(cudaStream_t stream,cudaError_t status, void*  userData ){
  timeStampD=getTimeStamp();
 }
+//initDataA function to init matrix A with selected values
 void initDataA(float* data, int nx, int ny){ 
  int i,j;
  for(i = 0; i < nx; i++){
@@ -28,6 +31,7 @@ void initDataA(float* data, int nx, int ny){
   }
  }
 }
+//initDataB function to init matrix B with selected values
 void initDataB(float* data, int nx, int ny){ 
  int i,j;
  for(i = 0; i < nx; i++){
@@ -36,6 +40,7 @@ void initDataB(float* data, int nx, int ny){
   }
  }
 }
+//for debug: print each element in matrix
 void debugPrint(float* data, int nx, int ny){
  int i,j;
  for(i = 0; i < nx; i++){
@@ -79,7 +84,7 @@ int main( int argc, char *argv[] ) {
  float *h_hA = (float *) malloc( bytes ) ;
  float *h_hB = (float *) malloc( bytes ) ;
  float *h_hC = (float *) malloc( bytes ) ; // host result
- //float *h_dC = (float *) malloc( bytes ) ; // gpu result
+ //the following allocate array pinned on memory to boost memcpy
  float *h_A, *h_B, *h_dC;
  float *d_A, *d_B ;
  cudaHostAlloc((void**)&h_A,bytes,cudaHostAllocWriteCombined|cudaHostAllocMapped);
@@ -97,24 +102,36 @@ int main( int argc, char *argv[] ) {
  double timeStampA = getTimeStamp() ;
 
  // invoke Kernel
- dim3 block( 32, 32 ) ; // you will want to configure this
+ dim3 block( 32, 32 ) ;
+ //I use 4 streams to pipeline memcpy h2d, kernal exec and memcpy d2h
+ //grid determines the number of blocks. "/4" means shrink into 1/4 of total size for kernal executing 4 elements addition
+ //"/NUM_STREAMS" means assign blocks to 4 streams uniformly
  int grid = ((noElems+3)/4/NUM_STREAMS + block.x*block.y-1)/(block.x*block.y);
+ //align_idx is to align 4 data in 32-byte data access for each kernal executing
  int align_idx = noElems/NUM_STREAMS-(noElems/NUM_STREAMS)%8;
 
+ //stream creation
  cudaStream_t stream[NUM_STREAMS+1];
  for (int i = 1; i < NUM_STREAMS+1; i++){
   cudaStreamCreate(&(stream[i]));
  }
  int i;
  for(i = 1; i < NUM_STREAMS; i++){
+  //async memcpy for A and B
   cudaMemcpyAsync(&d_A[(i-1)*align_idx],&h_A[(i-1)*align_idx],align_idx*sizeof(float),cudaMemcpyHostToDevice,stream[i]);
   cudaMemcpyAsync(&d_B[(i-1)*align_idx],&h_B[(i-1)*align_idx],align_idx*sizeof(float),cudaMemcpyHostToDevice,stream[i]);
+  //add callback to get timestamp update when each of memcpy per stream. I can get the last completion of data copying
   cudaStreamAddCallback(stream[i],myCallBackB,(void*)&i,0);
+  //kernal invoked
   f_addmat<<<grid, block, 0, stream[i]>>>( d_A+(i-1)*align_idx, d_B+(i-1)*align_idx,align_idx) ;
+  //add callback to get lastest stamp when finishing kernal per stream
   cudaStreamAddCallback(stream[i],myCallBackC,(void*)&i,0);
+  //async memcpy back to host
   cudaMemcpyAsync(&h_dC[(i-1)*align_idx],&d_B[(i-1)*align_idx],align_idx*sizeof(float),cudaMemcpyDeviceToHost,stream[i]);
+  //add callback to get lastest timestamp when finishing data copying
   cudaStreamAddCallback(stream[i],myCallBackD,(void*)&i,0);
  }
+ //Here is to run the last stream. It is out of loop since the size of remaing data is different from aligned data size
  grid =((noElems-(NUM_STREAMS-1)*align_idx+3)/4+ block.x*block.y-1)/(block.x*block.y);
  cudaMemcpyAsync(&d_A[(NUM_STREAMS-1)*align_idx],&h_A[(NUM_STREAMS-1)*align_idx],(noElems-(NUM_STREAMS-1)*align_idx)*sizeof(float),cudaMemcpyHostToDevice,stream[NUM_STREAMS]);
  cudaMemcpyAsync(&d_B[(NUM_STREAMS-1)*align_idx],&h_B[(NUM_STREAMS-1)*align_idx],(noElems-(NUM_STREAMS-1)*align_idx)*sizeof(float),cudaMemcpyHostToDevice,stream[NUM_STREAMS]);
@@ -123,6 +140,8 @@ int main( int argc, char *argv[] ) {
  cudaStreamAddCallback(stream[i],myCallBackC,(void*)&i,0);
  cudaMemcpyAsync(&h_dC[(NUM_STREAMS-1)*align_idx],&d_B[(NUM_STREAMS-1)*align_idx],(noElems-(NUM_STREAMS-1)*align_idx)*sizeof(float),cudaMemcpyDeviceToHost,stream[NUM_STREAMS]);
  cudaStreamAddCallback(stream[i],myCallBackD,(void*)&i,0);
+
+ //sync all streams and done
  for(int i = 1; i < NUM_STREAMS+1; i++){
   cudaStreamSynchronize(stream[i]);
  }
@@ -131,7 +150,7 @@ int main( int argc, char *argv[] ) {
  h_addmat( h_hA, h_hB, h_hC, nx, ny ) ;
 
  // print out results
- if(!memcmp(h_hC,h_dC,nx*ny*sizeof(float))){
+ if(!memcmp(h_hC,h_dC,nx*ny*sizeof(float))){//results compare
   //debugPrint(h_hC, nx, ny);
   //debugPrint(h_dC, nx, ny);
   //FILE* fptr;
